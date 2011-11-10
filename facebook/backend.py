493cb8d7a2cd2fe9f7434e6452aadb3fa28ff4dd
Vasyl Nesterenko <vasiliy.v.nesterenko@gmail.com>
2011-11-11 00:17:32 +0200

    all code has passed pep8 checker

--- facebook/backend.py
+++ facebook/backend.py
@@ -11,21 +11,26 @@ from facebook.models import FacebookProfile
 
 class FacebookBackend:
     def authenticate(self, token=None, request=None):
-        """ Reads in a Facebook code and asks Facebook if it's valid and what user it points to. """
+        """ Reads in a Facebook code and asks Facebook if it's valid and what
+        user it points to. """
         args = {
             'client_id': settings.FACEBOOK_APP_ID,
             'client_secret': settings.FACEBOOK_APP_SECRET,
-            'redirect_uri': request.build_absolute_uri('/facebook/authentication_callback'),
+            'redirect_uri': request.build_absolute_uri(
+                                        '/facebook/authentication_callback'),
             'code': token,
         }
 
         # Get a legit access token
-        target = urllib.urlopen('https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(args)).read()
+        target = urllib.urlopen(
+                        'https://graph.facebook.com/oauth/access_token?'
+                            + urllib.urlencode(args)).read()
         response = cgi.parse_qs(target)
         access_token = response['access_token'][-1]
 
         # Read the user's profile information
-        fb_profile = urllib.urlopen('https://graph.facebook.com/me?access_token=%s' % access_token)
+        fb_profile = urllib.urlopen(
+                'https://graph.facebook.com/me?access_token=%s' % access_token)
         fb_profile = json.load(fb_profile)
 
         try:
@@ -41,7 +46,8 @@ class FacebookBackend:
             # No existing user
 
             # Not all users have usernames
-            username = fb_profile.get('username', fb_profile['email'].split('@')[0])
+            username = fb_profile.get('username',
+                                      fb_profile['email'].split('@')[0])
 
             if getattr(settings, 'FACEBOOK_FORCE_SIGNUP', False):
                 # No existing user, use anonymous
@@ -59,16 +65,20 @@ class FacebookBackend:
                 # No existing user, create one
 
                 try:
-                    user = User.objects.create_user(username, fb_profile['email'])
+                    user = User.objects.create_user(username,
+                                                    fb_profile['email'])
                 except IntegrityError:
                     # Username already exists, make it unique
-                    user = User.objects.create_user(username + fb_profile['id'], fb_profile['email'])
+                    user = User.objects.create_user(
+                            username + fb_profile['id'], fb_profile['email'])
                 user.first_name = fb_profile['first_name']
                 user.last_name = fb_profile['last_name']
                 user.save()
 
                 # Create the FacebookProfile
-                fb_user = FacebookProfile(user=user, facebook_id=fb_profile['id'], access_token=access_token)
+                fb_user = FacebookProfile(user=user,
+                                          facebook_id=fb_profile['id'],
+                                          access_token=access_token)
                 fb_user.save()
 
         return user
